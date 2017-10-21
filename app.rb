4675d9c410b99bdf3cb694960878ab5f0c275c33
Chris Rudolph <chrisjames.r@gmail.com>
2017-10-21 13:55:04 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,4 +3,36 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
-end
+  get '/reversename/:name' do
+
+  end
+
+  get '/square/:number' do
+
+  end
+
+  get '/say/:number/:phrase' do
+
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+
+  end
+
+  get '/:operation/:number1/:number2' do
+    operation = params[:operation].downcase
+    num1 = params[:number1].to_i
+    num2 = params[:number2].to_i
+    case operation
+    when 'add'
+      num1 + num2
+    when 'subtract'
+      num1 - num2
+    when 'multiply'
+      num1 * num2
+    when 'divide'
+      num1 / num2
+    end      
+  end
+
+end  #  End of Class
